177f3ff271925108712438b0548d1bf9951853a5
Developer <developer@local>
2026-09-07 22:59:38 +0000

    feat: implementa CRUD de pacientes

---
+++ Controllers/PacientesController.cs
@@ -0,0 +1,100 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjetoAgendamento.Data;
+using ProjetoAgendamento.Models;
+
+namespace ProjetoAgendamento.Controllers;
+
+public class PacientesController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public PacientesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        List<Paciente> pacientes = _context.Pacientes.ToList();
+        return View(pacientes);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Paciente paciente)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(paciente);
+        }
+
+        _context.Pacientes.Add(paciente);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Edit(int id)
+    {
+        Paciente? paciente = _context.Pacientes.Find(id);
+
+        if (paciente == null)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(paciente);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(int id, Paciente paciente)
+    {
+        if (id != paciente.Id)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(paciente);
+        }
+
+        _context.Pacientes.Update(paciente);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Delete(int id)
+    {
+        Paciente? paciente = _context.Pacientes.Find(id);
+
+        if (paciente == null)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(paciente);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeleteConfirmed(int id)
+    {
+        Paciente? paciente = _context.Pacientes.Find(id);
+
+        if (paciente != null)
+        {
+            _context.Pacientes.Remove(paciente);
+            _context.SaveChanges();
+        }
+
+        return RedirectToAction(nameof(Index));
+    }
+}
